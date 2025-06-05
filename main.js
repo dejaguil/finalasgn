08b867c9119a05b6c1736ddf066a601566b0f11d
dereck <103078628+dejaguil@users.noreply.github.com>
2025-06-05 01:52:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/FBXLoader.js';
-import { GUI } from 'https://unpkg.com/lil-gui@0.18.2/dist/lil-gui.esm.min.js';
+import * as THREE from './three.module.js';
+import { OrbitControls } from './OrbitControls.js';
+import { FBXLoader } from './FBXLoader.js';
+import { GUI } from './lil-gui.js';
 
 //y
 
